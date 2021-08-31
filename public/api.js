7bce4f63cd4b3d84f0172c88722d6dbd6b277473
TMChambers <Tchaiwanda.chambers@gmail.com>
2021-08-31 19:37:05 -0400

    bug fix

--- public/api.js
+++ public/api.js
@@ -2,7 +2,7 @@ const API = {
   async getLastWorkout() {
     let res;
     try {
-      res = await fetch("/api/workouts");
+      res = await fetch("/api/workout");
     } catch (err) {
       console.log(err)
     }
@@ -11,20 +11,23 @@ const API = {
     return json[json.length - 1];
   },
   async addExercise(data) {
+    const id1 = location.search
+    const id2 = location.search.split("=")
     const id = location.search.split("=")[1];
 
-    const res = await fetch("/api/workouts/" + id, {
+    console.log(id1, id2, id)
+
+    const res = await fetch("/api/workout/" + id, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data)
     });
-
     const json = await res.json();
 
     return json;
   },
   async createWorkout(data = {}) {
-    const res = await fetch("/api/workouts", {
+    const res = await fetch("/api/workout", {
       method: "POST",
       body: JSON.stringify(data),
       headers: { "Content-Type": "application/json" }
