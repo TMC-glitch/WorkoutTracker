5fc21b6cf67e98f49af3aa43aaf85a5d1977a8de
TMChambers <Tchaiwanda.chambers@gmail.com>
2021-08-31 20:00:58 -0400

    uri

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const db = require("./models");
 const app = express();
+const uri = process.env.MONGODB_URI;
+
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
